60708f2594863f57a93132567b31662e3faee83f
GatoManzana <carlos_sanchez_m-tsapp1ma2324@gmail.com>
2024-10-15 10:34:29 +0100

    Initial Commit

---
+++ JuegoTareaIOSTests/JuegoTareaIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  JuegoTareaIOSTests.swift
+//  JuegoTareaIOSTests
+//
+//  Created by user234561 on 15/10/24.
+//
+
+import Testing
+@testable import JuegoTareaIOS
+
+struct JuegoTareaIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
